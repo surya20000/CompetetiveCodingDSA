bc1b5bac7d924fb01e04cf9ba76141a78f8028de
surya20000 <surya.p@kalvium.community>
2024-09-04 16:01:48 +0530

    Solved/FIrst Question

---
+++ Day7_Questions/RotateImages.py
@@ -0,0 +1,34 @@
+# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+#Question Link:- https://leetcode.com/problems/rotate-image/description/?envType=study-plan-v2&envId=top-interview-150
+
+# Solution Class
+
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        edge_length = len(matrix)
+
+        top = 0
+        bottom = edge_length - 1
+
+        while top < bottom:
+            for col in range(edge_length):
+                temp = matrix[top][col]
+                matrix[top][col] = matrix[bottom][col]
+                matrix[bottom][col] = temp
+            top += 1
+            bottom -= 1
+
+        for row in range(edge_length):
+            for col in range(row+1, edge_length):
+                temp = matrix[row][col]
+                matrix[row][col] = matrix[col][row]
+                matrix[col][row] = temp
+        
+        return matrix
