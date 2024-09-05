7b7c2531243f4f977426a1262000b91150703282
surya20000 <surya.p@kalvium.community>
2024-09-05 16:12:21 +0530

    Solved/Third Question

---
+++ Day8_Questions/ConatainsDuplicalte.py
@@ -0,0 +1,44 @@
+# Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1], k = 3
+# Output: true
+# Example 2:
+
+# Input: nums = [1,0,1,1], k = 1
+# Output: true
+# Example 3:
+
+# Input: nums = [1,2,3,1,2,3], k = 2
+# Output: false
+ 
+
+#Question Link:- https://leetcode.com/problems/contains-duplicate-ii/description/?envType=study-plan-v2&envId=top-interview-150
+ class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        visit = set()
+        
+        def get_next_number(n):    
+            output = 0
+            
+            while n:
+                digit = n % 10
+                output += digit ** 2
+                n = n // 10
+            
+            return output
+
+        while n not in visit:
+            visit.add(n)
+            n = get_next_number(n)
+            if n == 1:
+                return True
+        
+        return False        
