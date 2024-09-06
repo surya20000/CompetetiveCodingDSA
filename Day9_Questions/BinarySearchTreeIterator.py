a8e5535c545e91839072bc46d98beb6f35abe2da
surya20000 <surya.p@kalvium.community>
2024-09-06 15:44:07 +0530

    Solved/Fourth Question

---
+++ Day9_Questions/BinarySearchTreeIterator.py
@@ -0,0 +1,41 @@
+# Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+# BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+# boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+# int next() Moves the pointer to the right, then returns the number at the pointer.
+# Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+# You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+#Question Link:- https://leetcode.com/problems/binary-search-tree-iterator/description/?envType=study-plan-v2&envId=top-interview-150
+
+#Solution Class
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator(object):
+
+    def __init__(self, root):
+        self.stack = list()
+        self.pushAll(root)
+
+    def hasNext(self):
+        return self.stack
+    def next(self):
+        tmpNode = self.stack.pop()
+        self.pushAll(tmpNode.right)
+        return tmpNode.val
+        
+    def pushAll(self, node):
+        while node is not None:
+            self.stack.append(node)
+            node = node.left
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
