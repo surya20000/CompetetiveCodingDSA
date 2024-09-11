30069106f3a6e7aea5f65d634b7822fe14ada7da
surya20000 <surya.p@kalvium.community>
2024-09-11 14:39:06 +0530

    Solved/Palindrome_Number

---
+++ Day12_Questions/PalindromeNumber.py
@@ -0,0 +1,52 @@
+# Given an integer x, return true if x is a 
+# palindrome
+# , and false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: x = 121
+# Output: true
+# Explanation: 121 reads as 121 from left to right and from right to left.
+# Example 2:
+
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# Example 3:
+
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+# Constraints:
+
+# -231 <= x <= 231 - 1
+ 
+
+# Follow up: Could you solve it without converting the integer to a string?
+
+#Question Link:- https://leetcode.com/problems/palindrome-number/description/?envType=study-plan-v2&envId=top-interview-150
+
+#Solution class
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+
+        reversed_num = 0
+        temp = x
+
+        while temp != 0:
+            digit = temp % 10
+            reversed_num = reversed_num * 10 + digit
+            temp //= 10
+
+        return reversed_num == x
