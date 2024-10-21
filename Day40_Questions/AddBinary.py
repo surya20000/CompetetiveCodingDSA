59bdb1936238bbc5e3aa01dee33ac5917f39ad0a
surya20000 <surya.p@kalvium.community>
2024-10-21 09:36:07 +0530

    Solved/Add Binary

---
+++ Day40_Questions/AddBinary.py
@@ -0,0 +1,34 @@
+# Question Link:- https://leetcode.com/problems/add-binary/description/?envType=study-plan-v2&envId=top-interview-150
+
+# Given two binary strings a and b, return their sum as a binary string.
+
+# Example 1:
+
+# Input: a = "11", b = "1"
+# Output: "100"
+# Example 2:
+
+# Input: a = "1010", b = "1011"
+# Output: "10101"
+
+# Solution Class
+
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        res = ""
+        i, j, carry = len(a) - 1, len(b) - 1, 0
+        while i >= 0 or j >= 0:
+            sum = carry
+            if i >= 0 : sum += ord(a[i]) - ord('0')
+            if j >= 0 : sum += ord(b[j]) - ord('0')
+            i, j = i - 1, j - 1
+            carry = 1 if sum > 1 else 0
+            res += str(sum % 2)
+
+        if carry != 0 : res += str(carry)
+        return res[::-1]
