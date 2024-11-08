555af29b2e3fdbd3070c243ecaa0efa24bd3705a
surya20000 <surya.p@kalvium.community>
2024-11-08 09:48:24 +0530

    Solved/IsSubsequence

--- Day50_Questions/IsSubSequence.py
+++ Day50_Questions/IsSubSequence.py
@@ -17,3 +17,10 @@
 
 # Solution 
 
+def isSubsequence(s,t):
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+        if s[i] == j[t]:
+            i += 1
+        j += 1
+    return i == len(s)
