b374dd46e8104bd5b4697826f55f2c2586d8fcd1
surya20000 <surya.p@kalvium.community>
2024-11-13 13:48:39 +0530

    Solved/Max Consecutive Ones III

--- Day56_Questions/MaximumConsecutiveOnes.py
+++ Day56_Questions/MaximumConsecutiveOnes.py
@@ -16,3 +16,15 @@
 # Output: 10
 # Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
 # Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+# Solved
+def longestOnes(nums, k):
+    left = 0
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            k -= 1
+        if k < 0:
+            if nums[left] == 0:
+                k += 1
+            left += 1
+    return right - left + 1
