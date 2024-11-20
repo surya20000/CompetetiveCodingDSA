d596ebad7c22e63f692eabe2bea523cb4f7ab2fe
surya20000 <surya.p@kalvium.community>
2024-11-20 11:49:30 +0530

    Solved/Count Good Nodes In Binary Tree

--- Day62_Questions/CountGoodNodesInBinaryTree.py
+++ Day62_Questions/CountGoodNodesInBinaryTree.py
@@ -30,3 +30,30 @@
 # Output: 1
 # Explanation: Root is considered as good.
 
+# Solution 
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def goodNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        def dfs(node, curMax):
+            if not node:
+                return
+            if node.val >= curMax:
+                count[0] += 1
+                curMax = node.val
+            dfs(node.left, curMax)
+            dfs(node.right, curMax)
+        count = [0]
+        dfs(root, root.val)
+        return count[0]
