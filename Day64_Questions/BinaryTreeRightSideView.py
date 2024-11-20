f9b31e8d994e3bc7b8a3ac5dac43fa83614d1ef2
surya20000 <surya.p@kalvium.community>
2024-11-20 13:18:54 +0530

    Solved/Binary Tree Right Side View

--- Day64_Questions/BinaryTreeRightSideView.py
+++ Day64_Questions/BinaryTreeRightSideView.py
@@ -17,3 +17,24 @@
 
 # Input: root = []
 # Output: []
+
+# Solution 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        d=defaultdict(int)
+        def foo(x,depth):
+            if x.left: foo(x.left,depth+1)
+            d[depth]=x.val
+            if x.right: foo(x.right,depth+1)
+        if root: foo(root,0)
+        return [d[i] for i in sorted(d.keys())]
