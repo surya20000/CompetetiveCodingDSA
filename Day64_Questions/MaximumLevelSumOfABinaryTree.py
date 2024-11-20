35470f34daf1409d576faa18aa7f0e10be3fdd31
surya20000 <surya.p@kalvium.community>
2024-11-20 13:22:30 +0530

    Solved/Maximum Level Sum of a Binary Tree

--- Day64_Questions/MaximumLevelSumOfABinaryTree.py
+++ Day64_Questions/MaximumLevelSumOfABinaryTree.py
@@ -21,4 +21,39 @@
 # Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
 # Output: 2
 
-
+# Solution 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxLevelSum(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if root.left is None and root.right is None:
+            return 1
+        def BFS(root):
+            q=deque([root])
+            maximum=[]
+            maximum.append(root.val)
+            while q:
+                l=len(q)
+                value=0
+                for i in range(l):
+                    tempRoot=q.popleft()
+                    if tempRoot.left:
+                        q.append(tempRoot.left)
+                        value+=tempRoot.left.val
+                    if tempRoot.right:
+                        q.append(tempRoot.right)
+                        value+=tempRoot.right.val
+                print(maximum)
+                maximum.append(value)
+            maximum.pop()
+            print(maximum)
+            return maximum.index(max(maximum))+1
+        return BFS(root)
