ea14d15dd96931a0490fbc3c808b5d467b924b41
surya20000 <surya.p@kalvium.community>
2024-11-20 13:37:39 +0530

    Solved/Search in a Binary Search Tree

--- Day65_Questions/SearchInABinarySearchTree.py
+++ Day65_Questions/SearchInABinarySearchTree.py
@@ -16,3 +16,27 @@
 
 # Input: root = [4,2,7,1,3], val = 5
 # Output: []
+
+# Solution 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return None
+        
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
