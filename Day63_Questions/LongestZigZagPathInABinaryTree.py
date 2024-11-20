c784b71681ca57f58716d1f203c73ad72fd5eb0d
surya20000 <surya.p@kalvium.community>
2024-11-20 12:21:34 +0530

    Solved/Longest Zig Zag Path In A Binary Tree

--- Day63_Questions/LongestZigZagPathInABinaryTree.py
+++ Day63_Questions/LongestZigZagPathInABinaryTree.py
@@ -31,3 +31,35 @@
 # Input: root = [1]
 # Output: 0
 
+# Solution 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def longestZigZag(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        self.max_length = 0
+        
+        def dfs(node, is_left, length):
+            if not node:
+                return
+            
+            self.max_length = max(self.max_length, length)
+            
+            if is_left:
+                dfs(node.right, False, length + 1) 
+                dfs(node.left, True, 1) 
+            else:
+                dfs(node.left, True, length + 1)
+                dfs(node.right, False, 1)
+        
+        dfs(root.left, True, 1) 
+        dfs(root.right, False, 1) 
+        
+        return self.max_length
