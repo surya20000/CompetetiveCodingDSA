fd02872711179786df8358c9c5d1840eaa949603
surya20000 <surya.p@kalvium.community>
2024-11-20 11:53:44 +0530

    Solved/Path Sum III

--- Day62_Questions/PathSumiii.py
+++ Day62_Questions/PathSumiii.py
@@ -16,3 +16,36 @@
 
 # Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
 # Output: 3
+
+# Solution
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def pathSum(self, root, target):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: int
+        """
+        self.numOfPaths = 0
+        self.dfs(root, target)
+        return self.numOfPaths
+    
+    def dfs(self, node, target):
+        if node is None:
+            return 
+        self.test(node, target)
+        self.dfs(node.left, target)
+        self.dfs(node.right, target)
+        
+    def test(self, node, target):
+        if node is None:
+            return
+        if node.val == target:
+            self.numOfPaths += 1
+        self.test(node.left, target-node.val)
+        self.test(node.right, target-node.val)
