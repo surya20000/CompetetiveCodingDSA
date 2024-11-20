b95d46e7c5691203dad16df9ba3b5c12ed639c12
surya20000 <surya.p@kalvium.community>
2024-11-20 11:37:19 +0530

    Solved/Leaf similar Trees

--- Day61_Questions/LeafSimilarTrees.py
+++ Day61_Questions/LeafSimilarTrees.py
@@ -22,3 +22,34 @@
 
 # Input: root1 = [1,2,3], root2 = [1,3,2]
 # Output: false
+
+# Solution 
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: Optional[TreeNode]
+        :type root2: Optional[TreeNode]
+        :rtype: bool
+        """
+        def leaves(root):
+            s1 = []
+            leaf = []
+            s1.append(root)
+
+            while len(s1) != 0:
+                curr = s1.pop()
+                if curr.left:
+                    s1.append(curr.left)
+                if curr.right:
+                    s1.append(curr.right)
+                if not curr.left and not curr.right:
+                    leaf.append(curr.val)
+            return leaf
+
+        return leaves(root1) == leaves(root2)
